d419ec9076670547985248e256fc597c161ea4e8
Jin Igarashi <igarashi@mierune.co.jp>
2021-03-26 15:55:47 +0900

    added  for nominatim import query in entrypoint.sh

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
+if [ ! -n "$MAX_LOCAL_STORAGE_NODES" ]; then
+  MAX_LOCAL_STORAGE_NODES = 1
+fi
+
 # Create elasticsearch index
 if [ ! -d "/photon/photon_data/elasticsearch" ]; then
 	echo "Creating search index"
 	if [ -n "$PHOTON_LANGUAGES" ]; then
-    java -Xms7G -Xmx7G -jar photon.jar -nominatim-import -host $NOMINATIM_HOST -port $NOMINATIM_PORT -database $NOMINATIM_DB_NAME -user $NOMINATIM_USER -password $NOMINATIM_PASSWORD -languages $PHOTON_LANGUAGES
+    java -Xms7G -Xmx7G -jar photon.jar -max-local-storage-nodes $MAX_LOCAL_STORAGE_NODES -nominatim-import -host $NOMINATIM_HOST -port $NOMINATIM_PORT -database $NOMINATIM_DB_NAME -user $NOMINATIM_USER -password $NOMINATIM_PASSWORD -languages $PHOTON_LANGUAGES
   else
-    java -Xms7G -Xmx7G -jar photon.jar -nominatim-import -host $NOMINATIM_HOST -port $NOMINATIM_PORT -database $NOMINATIM_DB_NAME -user $NOMINATIM_USER -password $NOMINATIM_PASSWORD
+    java -Xms7G -Xmx7G -jar photon.jar -max-local-storage-nodes $MAX_LOCAL_STORAGE_NODES -nominatim-import -host $NOMINATIM_HOST -port $NOMINATIM_PORT -database $NOMINATIM_DB_NAME -user $NOMINATIM_USER -password $NOMINATIM_PASSWORD
   fi
-fi
-
-if [ ! -n "$MAX_LOCAL_STORAGE_NODES" ]; then
-  MAX_LOCAL_STORAGE_NODES = 1
 fi
 
 # Start photon if elastic index exists
