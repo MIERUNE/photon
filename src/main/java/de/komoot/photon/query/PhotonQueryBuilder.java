734003ae34e823e9adfa5a566698f38d805295a3
Jin Igarashi <igarashi@mierune.co.jp>
2021-05-19 15:04:56 +0900

    excluded other languages from multimatchquery

--- src/main/java/de/komoot/photon/query/PhotonQueryBuilder.java
+++ src/main/java/de/komoot/photon/query/PhotonQueryBuilder.java
@@ -92,7 +92,12 @@ public class PhotonQueryBuilder {
             MultiMatchQueryBuilder builder =
                     QueryBuilders.multiMatchQuery(query).field(defaultCollector, 1.0f).type(MultiMatchQueryBuilder.Type.CROSS_FIELDS).prefixLength(2).analyzer(defaultNgramAnalyzer).minimumShouldMatch("100%");
 
+            String[] excludesLang = { "ja" };
             for (String lang : languages) {
+                if(!Arrays.asList(excludesLang).contains(language)){
+                    // exclude other language for particular multibyte languages
+                    continue;
+                }
                 builder.field(String.format("collector.%s.ngrams", lang), lang.equals(language) ? 1.0f : 0.6f);
             }
 
