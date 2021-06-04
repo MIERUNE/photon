db47f5e86a77d6ea3891263d32b7b8e62fd31c45
kenseii <oasisagano@gmail.com>
2021-06-04 12:00:15 +0900

    use new synonyms and apply expand

--- src/main/java/de/komoot/photon/query/PhotonQueryBuilder.java
+++ src/main/java/de/komoot/photon/query/PhotonQueryBuilder.java
@@ -83,8 +83,8 @@ public class PhotonQueryBuilder {
         }
 
         BoolQueryBuilder synonymsQuery = QueryBuilders.boolQuery()
-    .must(QueryBuilders.matchQuery("osm_key_text", query).analyzer("synonym_analyzer"))
-    .must(QueryBuilders.matchQuery("osm_value_text", query).analyzer("synonym_analyzer"));
+    .must(QueryBuilders.matchQuery("osm_key_text", query).boost(100).analyzer("synonym_analyzer"))
+    .must(QueryBuilders.matchQuery("osm_value_text", query).boost(200).analyzer("synonym_analyzer"));
 
         query4QueryBuilder
                 .should(QueryBuilders.matchQuery(String.format("name.%s.raw", language), query).boost(200)
