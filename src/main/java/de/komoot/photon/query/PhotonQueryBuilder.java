3f9d888baad0ee3f75f82a0f98a8ab0084931f04
Jin Igarashi <igarashi@mierune.co.jp>
2021-06-17 16:43:15 +0900

    deleted multimatchphrase

--- src/main/java/de/komoot/photon/query/PhotonQueryBuilder.java
+++ src/main/java/de/komoot/photon/query/PhotonQueryBuilder.java
@@ -92,31 +92,11 @@ public class PhotonQueryBuilder {
 
             collectorQuery = builder;
         } else {
-            MultiMatchQueryBuilder builderDefault =
-                    QueryBuilders.multiMatchQuery(query).field(Arrays.asList(cjkLanguages).contains(language) ? String.format("collector.default.ngrams_%s", language): "collector.default.ngrams", 1.0f).type(MultiMatchQueryBuilder.Type.CROSS_FIELDS).prefixLength(2).analyzer("search_ngram").minimumShouldMatch("100%");
+            MultiMatchQueryBuilder builder =
+                    QueryBuilders.multiMatchQuery(query).field("collector.default.ngrams", 1.0f).type(MultiMatchQueryBuilder.Type.CROSS_FIELDS).prefixLength(2).analyzer("search_ngram").minimumShouldMatch("100%");
 
             for (String lang : languages) {
-                builderDefault.field(String.format("collector.%s.ngrams", lang), lang.equals(language) ? 1.0f : 0.6f);
-            }
-
-            BoolQueryBuilder builder = QueryBuilders.boolQuery().should(builderDefault);
-
-            if (Arrays.asList(cjkLanguages).contains(language)) {
-                MultiMatchQueryBuilder builderJapaneseField =
-                        QueryBuilders.multiMatchQuery(query)
-                                .field(String.format("collector.default.raw_%s",language), 1.0f)
-                                .type(MultiMatchQueryBuilder.Type.PHRASE)
-                                .prefixLength(2)
-                                .slop(1)
-//                                .operator(Operator.AND)
-//                                .analyzer(String.format("%s_search_raw", language))
-                                .minimumShouldMatch("100%");
-
-                for (String lang : languages) {
-                    builderJapaneseField.field(String.format("collector.%s.raw", lang), lang.equals(language) ? 1.0f : 0.6f);
-                }
-
-                builder = builder.should(builderJapaneseField);
+                builder.field(String.format("collector.%s.ngrams", lang), lang.equals(language) ? 1.0f : 0.6f);
             }
 
             collectorQuery = builder;
